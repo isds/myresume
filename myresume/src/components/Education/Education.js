7baa70e21190257395805eeb6970c69a495238cf
Ítalo Silva <italosilvadesouza@gmail.com>
2019-02-21 04:55:43 -0300

    Fixed Education section alignment and text size.

--- myresume/src/components/Education/Education.js
+++ myresume/src/components/Education/Education.js
@@ -7,30 +7,22 @@ class Education extends Component {
   render() {
     return (
       <div id="education" className="education-container">
-        <div>
-          <span className="education-title">
+        <div className="education-title">
+          <span>
             Education
           </span>
-
-          <div className="job-card-divisor"></div>
-
-          <div>
-            <div className="education-card">
-              <strong>EEEP Paulo Petrola</strong>
-              <p>Information Technology Technician</p>
-            </div>
-          </div>
-
-          <div className="education-card-divisor"></div>
-
-          <div>
-            <div className="education-card">
-              <strong>Greate Fortaleza Metropolitan University</strong>
-              <p>Analysis Systems Development</p>
-            </div>
-          </div>
-
         </div>
+
+        <div className="education-card">
+          <strong>EEEP Paulo Petrola</strong>
+          <p>Information Technology Technician</p>
+        </div>
+
+        <div className="education-card">
+          <strong>Greate Fortaleza Metropolitan University</strong>
+          <p>Analysis Systems Development</p>
+        </div>
+
       </div>
     );
   }
