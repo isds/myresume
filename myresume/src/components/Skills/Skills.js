c48ec13fde56437935147c161b86e32093b5afa8
Ítalo Silva <italosilvadesouza@gmail.com>
2019-02-22 17:15:19 -0300

    Removing bootstrap classes, making use of grid and flexbox.

--- myresume/src/components/Skills/Skills.js
+++ myresume/src/components/Skills/Skills.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import './Skills.css';
 
+const skills = [
+  "HTML5", "CSS3", "JavaScript", "ReactJS", "C", "VBA", "Python", "Git", "MS Access", "Informix",
+  "MySql", "PostgreSQL", "MongoDB", "Amazon Redshift", "Amazon RDS", "Aerospike"
+];
 
 class Skills extends Component {
   render() {
-    const skills = [
-      "HTML5", "CSS3", "JavaScript", "ReactJS", "C", "VBA", "Python", "Git", "MS Access", "Informix",
-      "MySql", "PostgreSQL", "MongoDB", "Amazon Redshift", "Amazon RDS", "Aerospike"
-    ];
     const skillItems = skills.map(
       (skill, i) => { return <label key={i} className="skill-item">{skill}</label> }
     );
 
     return (
       <div id="skills" className="skills-container">
-        <div >
-          <span className="skills-title">
+        <div className="skills-title">
+          <span >
             Skills
           </span>
-
-          <div>
-            <div className="skills-card">
-              {skillItems}
-            </div>
+        </div>
+        <div>
+          <div className="skills-card">
+            {skillItems}
           </div>
         </div>
       </div>
