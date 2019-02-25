6a3ef661926d1c7f5af9d53b05f18e035368df57
Ítalo Silva <italosilvadesouza@gmail.com>
2019-02-25 11:19:27 -0300

    Created Sidedrawer with responsive functionalities.

--- myresume/src/components/App/App.js
+++ myresume/src/components/App/App.js
@@ -8,16 +8,38 @@ import About from '../About/About';
 import Education from '../Education/Education';
 import Experience from '../Experience/Experience';
 import Skills from '../Skills/Skills';
-import Contact from '../Contact/Contact'
+import Contact from '../Contact/Contact';
+import Backdrop from '../Backdrop/Backdrop';
 
 import './App.css';
+import SideDrawer from '../SideDrawer/SideDrawer';
 
 
 class App extends Component {
+  state = {
+    sideDrawerOpen: false,
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !this.state.sideDrawerOpen };
+    })
+  };
+
+  backDropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false })
+  };
+
   render() {
+    let backdrop;
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backDropClickHandler} />;
+    }
     return (
       <div>
-        <Menu />
+        <Menu drawerClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen}/>;
+        {backdrop}
         <Header />
         <About />
         <Education />
--- myresume/src/components/SideDrawer/SideDrawer.js
+++ myresume/src/components/SideDrawer/SideDrawer.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 import './SideDrawer.css';
 
-const SideDrawer = (pros) => (
-  <nav className="side-drawer">
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#education">Education</a></li>
-      <li><a href="#experience">Experience</a></li>
-      <li><a href="#skills">Skills</a></li>
-    </ul>
-  </nav>
-);
+const SideDrawer = (props) => {
+  let drawerClasses = 'side-drawer';
+  if(props.show) {
+    drawerClasses = 'side-drawer open';
+  }
+  return (
+    <nav className={drawerClasses}>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#skills">Skills</a></li>
+      </ul>
+    </nav>
+  )
+};
 
 export default SideDrawer;
