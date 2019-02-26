b5cbcacd6be0058eb2767a25fffa5b29791b13d3
Ítalo Silva <italosilvadesouza@gmail.com>
2019-02-26 00:58:34 -0300

    Updates

---
+++ precache-manifest.827d4963d51c07e03981dbbf1c953937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82b70cd3742f21482d55",
+    "url": "/myresume/static/js/runtime~main.82b70cd3.js"
+  },
+  {
+    "revision": "78a6b5b481fd38b7abc7",
+    "url": "/myresume/static/js/main.78a6b5b4.chunk.js"
+  },
+  {
+    "revision": "c1c74dd19bf6ef05bed3",
+    "url": "/myresume/static/js/1.c1c74dd1.chunk.js"
+  },
+  {
+    "revision": "78a6b5b481fd38b7abc7",
+    "url": "/myresume/static/css/main.18e0717b.chunk.css"
+  },
+  {
+    "revision": "c1c74dd19bf6ef05bed3",
+    "url": "/myresume/static/css/1.42fc6fb0.chunk.css"
+  },
+  {
+    "revision": "b343ff62e4f4015ab8c47efa036b8787",
+    "url": "/myresume/index.html"
+  }
+];
